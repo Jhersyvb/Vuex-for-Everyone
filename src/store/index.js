9442cb5cb72b0594fe1157b0cf4511b782328f92
Jhersy <jhersyvb@gmail.com>
2020-04-08 19:10:06 -0500

    3 Use Vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,30 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
-});
+  state: {
+    // = data
+    products: []
+  },
+
+  getters: {
+    // = computed properties
+    productsCount() {
+      //
+    }
+  },
+
+  actions: {
+    fetchProducts() {
+      // make the call
+    }
+  },
+
+  mutations: {
+    setProducts() {
+      // update products
+    }
+  }
+})
